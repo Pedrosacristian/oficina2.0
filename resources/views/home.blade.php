ef2c10a2206b637569d4a590d5a1008bcd370ea9
Cristian Pedrosa <100144418+Pedrosacristian@users.noreply.github.com>
2022-12-13 19:49:33 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,12 @@
-// Tela incial do sistema
+
 <body>
         <h1> Oficina 2.0 </h1>
         <div> 
             <ul class= 'navbar-nav'>
                 <li class='nav-item'>
-                // Saída para atela de cadastro
                     <a href='/orcamento/create' class='nav-link'> Cadastrar Orçamento</a>
                 </li>  
                 <li class='nav-item'>
-                // Saída para atela de busca, edição e exclusão
                     <a href='/orcamento/busca' class='nav-link'> Buscar orçamentos</a>
                 </li>
             </ul>
